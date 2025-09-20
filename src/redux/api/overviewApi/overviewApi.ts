df2609a39706faa53a4f5a7d439c44cbecdbbcd5
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-09-20 17:58:29 +0600

    user page data fetched

--- src/redux/api/overviewApi/overviewApi.ts
+++ src/redux/api/overviewApi/overviewApi.ts
@@ -1,6 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { baseApi } from "../baseApi";
 
+interface GetAllUsersParams {
+  role?: "ALL" | "STUDENT" | "TEACHER" | "PARENT"; // restrict roles
+  search_term?: string;
+  page?: number;
+}
+
+interface Meta {
+  total_item: number;
+  total_page: number;
+  limit: number;
+  page: number;
+}
+
+interface UserData {
+  _id: string;
+  email: string;
+  role: string;
+  full_name: string;
+  nick_name: string;
+  date_of_birth: string;
+  phone: string;
+  address: string;
+  image: string;
+  avatar_id: string;
+  gender: string;
+  parent?: string;
+}
+
+interface GetAllUsersResponse {
+  meta: Meta;
+  data: UserData[];
+}
+
 export const overviewApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getRecentUsers: builder.query<
@@ -15,15 +48,24 @@ export const overviewApi = baseApi.injectEndpoints({
         });
         return `/dashboard/overview-recent-user?${params.toString()}`;
       },
-      providesTags: ["privacy"],
     }),
 
     getTotalCount: builder.query<any, void>({
       query: () => "/dashboard/overview-total",
-      providesTags: ["privacy"],
+    }),
+    getAllUsers: builder.query<GetAllUsersResponse, GetAllUsersParams>({
+      query: (params) => ({
+        url: "/dashboard/get-all-user",
+        method: "GET",
+        params, // typed params
+      }),
     }),
   }),
   overrideExisting: false,
 });
 
-export const { useGetRecentUsersQuery, useGetTotalCountQuery } = overviewApi;
+export const {
+  useGetRecentUsersQuery,
+  useGetTotalCountQuery,
+  useGetAllUsersQuery,
+} = overviewApi;
